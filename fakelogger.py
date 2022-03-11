75a356ae39840aa52aa84925830043b87b2500b2
omkardamame <omkardamame.work@gmail.com>
2022-03-11 19:25:20 +0530

    V4

--- fakelogger.py
+++ fakelogger.py
@@ -6,6 +6,6 @@ while n!=0:
     for x in range(l):
         t = time.localtime()
         current_time = time.strftime("%H:%M:%S", t)
-        print (current_time,'This is some random log.')  
+        print ("This is some random text and today's time is ",current_time)  
     n-=1    
     time.sleep(1)
