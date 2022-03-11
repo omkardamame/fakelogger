83eb182e3ee249cc90f90b6da47ce9dc230f7037
omkardamame <omkardamame.work@gmail.com>
2022-03-11 19:15:53 +0530

    V3

--- fakelogger.py
+++ fakelogger.py
@@ -2,10 +2,10 @@ import time
 
 n = int(input("Enter Interval: "))
 l = int(input("Enter No. of lines to Show at a time: "))
-t = time.localtime()
-current_time = time.strftime("%H:%M:%S", t)
 while n!=0:
     for x in range(l):
+        t = time.localtime()
+        current_time = time.strftime("%H:%M:%S", t)
         print (current_time,'This is some random log.')  
     n-=1    
     time.sleep(1)
